c3c02e8bcc1552edf3b74617a267334b81541c17
Fadhlan Sulistiyo Hidayat <fadhlansulistiyo@gmail.com>
2021-06-05 07:35:32 +0700

    Finishing Networking

--- app/src/main/java/com/dicoding/mymovies/data/source/remote/response/GenresItem.kt
+++ app/src/main/java/com/dicoding/mymovies/data/source/remote/response/GenresItem.kt
@@ -3,9 +3,8 @@ package com.dicoding.mymovies.data.source.remote.response
 import com.google.gson.annotations.SerializedName
 
 data class GenresItem(
-    @field:SerializedName("name")
-    val name: String,
-
-    @field:SerializedName("id")
-    val id: Int
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String
 )
--- app/src/main/java/com/dicoding/mymovies/network/ApiService.kt
+++ app/src/main/java/com/dicoding/mymovies/network/ApiService.kt
@@ -1,6 +1,9 @@
 package com.dicoding.mymovies.network
 
+import com.dicoding.mymovies.data.source.remote.response.DetailMoviesResponse
+import com.dicoding.mymovies.data.source.remote.response.DetailTvShowResponse
 import com.dicoding.mymovies.data.source.remote.response.MoviesResponse
+import com.dicoding.mymovies.data.source.remote.response.TVShowResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -14,8 +17,20 @@ interface ApiService {
     ) : Call<MoviesResponse>
 
     @GET("movie/{id}")
-    fun getMoviesDetail(
+    fun getDetailMovies(
         @Path("id") id: String,
         @Query("api_key") apiKey: String
-    ) : Call<MoviesDe>
+    ) : Call<DetailMoviesResponse>
+
+    @GET("discover/tv")
+    fun getTvShow(
+        @Query("api_key") apiKey: String
+    ) : Call<TVShowResponse>
+
+    @GET("tv/{id}")
+    fun getDetailTvShow(
+        @Path("id") id: String,
+        @Query("api_key") apiKey: String
+    ) : Call<DetailTvShowResponse>
+
 }
